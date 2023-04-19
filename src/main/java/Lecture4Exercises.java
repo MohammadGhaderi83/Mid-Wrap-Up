c31c20f6c7f71b5c1b40489fd29c3b84a74e1c10
MohammadGhaderi83 <mohammadghaderi181@gmail.com>
2023-04-20 00:37:49 +0430

    Warp uo assignement is finish

--- src/main/java/Lecture4Exercises.java
+++ src/main/java/Lecture4Exercises.java
@@ -1,52 +1,78 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lecture4Exercises {
-
-    /*
-     *   implement a function that returns factorial of given n
-     *   lecture 4 page 15
-     */
+    private static Map<Integer, Long> factorial = new HashMap<>();
+    private static Map<Integer, Long> fibo = new HashMap<>();
     public long factorial(int n) {
-        return 0L;
+        if (n == 0)
+        {
+            return 1;
+        } else if (factorial.containsKey(n))
+        {
+            return factorial.get(n);
+        } else {
+            Long result = n * factorial(n-1);
+            factorial.put(n, result);
+            return result;
+        }
     }
 
-    /*
-     *   implement a function that return nth number of fibonacci series
-     *   the series -> 1, 1, 2, 3, 5, 8, ...
-     *   lecture 4 page 19
-     */
     public long fibonacci(int n) {
-        return 0;
+        if (n <= 1){
+            return n;
+        } else if (fibo.containsKey(n)) {
+            return fibo.get(n);
+        } else {
+            long result = fibonacci(n-1) + fibonacci(n-2);
+            fibo.put(n, result);
+            return result;
+        }
     }
 
-    /*
-     *   implement a function that return reverse of a given word
-     *   lecture 4 page 19
-     */
     public String reverse(String word) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = word.length()-1; i >= 0 ; i--){
+            result.append(word.charAt(i));
+        }
+        return result.toString();
     }
 
-    /*
-     *   implement a function that returns true if the given line is
-     *   palindrome and false if it is not palindrome.
-     *   palindrome is like 'wow', 'never odd or even', 'Wow'
-     *   lecture 4 page 19
-     */
     public boolean isPalindrome(String line) {
-        return false;
+        int i = 0;
+        int j = line.length() - 1;
+        while (i < j) {
+            char leftChar = Character.toLowerCase(line.charAt(i));
+            char rightChar = Character.toLowerCase(line.charAt(j));
+            if (!Character.isLetterOrDigit(leftChar)) {
+                i++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                j--;
+            } else if (leftChar != rightChar) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
     }
 
-    /*
-     *   implement a function which computes the dot plot of 2 given
-     *   string. dot plot of hello and ali is:
-     *       h e l l o
-     *   h   *
-     *   e     *
-     *   l       * *
-     *   l       * *
-     *   o           *
-     *   lecture 4 page 26
-     */
+
     public char[][] dotPlot(String str1, String str2) {
-        return null;
+        int len1 = str1.length();
+        int len2 = str2.length();
+        char[][] result = new char[len1][len2];
+
+        for (int i = 0 ; i < len1 ; i++){
+            for (int j = 0 ; j < len2; j++){
+                if (str1.charAt(i) == str2.charAt(j)){
+                    result[i][j] = '*';
+                } else {
+                    result[i][j] = ' ';
+                }
+            }
+        }
+        return result;
     }
 }
